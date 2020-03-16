a8ef6053088b41ab8756693cb7ac0fc47bbb5fd5
hanjs92 <hanjs92@naver.com>
2020-03-16 23:32:28 +0900

    업데이트등 의 component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const foodILike =[{id:1,name:"kimchi",image:"aaa",rating:3.2},{id:2,name:"ramen",image:"bbb",rating:3},{id:3,name:"potato",image:"ccc",rating:1}];
-
-// function renderFood(dish){
-//   return <Food name={dish.name} picture={dish.picture} />
-// }
-
-function Food({name, picture,rating}){
-  return (
-  <div> 
-  <h3>I love {name} </h3>
-  <p>{picture}</p>
-  <h4>{rating}/5.0</h4>
-  <hr></hr>
-  </div>
-  );
-}
-
-Food.propTypes = {
- name: PropTypes.string.isRequired,
- picture: PropTypes.string.isRequired,
- rating: PropTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div>
-      <h1>HELLO!!!</h1>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} /> )/* == {foodILike.map(renderFood)} */}
-      
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hel")
+  }
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current =>({count: current.count + 1}))
+  };
+  minus = () =>{
+    this.setState(current =>({count:current.count - 1}))
+  };
+  // mount
+  componentDidMount(){
+    console.log("component render")
+  }
+  //update
+  componentDidUpdate(){
+    console.log("I just updated")
+  }
+  // Unmount //component가 떠날때 호출 (ex}다른페이지이동시)
+  componentWillUnmount(){
+    console.log("Good Bye, cruel World")
+  }
+  render(){
+    console.log("bye");
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
